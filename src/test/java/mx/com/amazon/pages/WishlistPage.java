e21b0daf97cc6ca8fe93ebf73b2fb21ad76c624b
Gabriel Duran <arcgabriel@outlook.com>
2020-07-06 17:28:48 -0500

    V1.1

--- src/test/java/mx/com/amazon/pages/WishlistPage.java
+++ src/test/java/mx/com/amazon/pages/WishlistPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -20,6 +22,10 @@ public class WishlistPage extends BasePage
     @FindBy(css = "[data-action='reg-nav-link'] div div span")
     List<WebElement> lists;
     
+    @FindBy(id = "your-lists-nav")
+    WebElement myLists;
+    
+    
     public boolean isAtWishListPage()
     {
         return lists.size() > 0;
@@ -27,6 +33,8 @@ public class WishlistPage extends BasePage
     
     public boolean verifyWishListCreated(String wishList)
     {
+        driver.navigate().refresh();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(myLists));
         for(WebElement list : lists )
         {
             if (list.getText().equals(wishList))
